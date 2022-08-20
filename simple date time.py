1a95e32afbe722fb262d4f5acd7d62ac71d8d2f4
Dreadwolf26 <107774155+Dreadwolf26@users.noreply.github.com>
2022-08-20 08:18:18 -0400

    Add files via upload

---
+++ simple date time.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time: ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
